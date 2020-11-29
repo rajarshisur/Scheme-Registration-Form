274ef0d431f3722f593f6d953f77c7bfc93f4648
Rajarshi <58479147+rajarshisur@users.noreply.github.com>
2020-11-29 21:44:01 +0530

    Add files via upload

---
+++ build/precache-manifest.4a676aa9bd034c1122bc261b8c5f1ab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8da56d8b0a72fa7986cdef8ad7ab9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32029127c2d4e00241dd",
+    "url": "/static/css/2.14ab0cfd.chunk.css"
+  },
+  {
+    "revision": "59ed63e88fa32e23667d",
+    "url": "/static/css/main.1b13a6a8.chunk.css"
+  },
+  {
+    "revision": "32029127c2d4e00241dd",
+    "url": "/static/js/2.ab189bfc.chunk.js"
+  },
+  {
+    "revision": "f59da382ef0e40e6fd7e14f0c7baae9f",
+    "url": "/static/js/2.ab189bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ed63e88fa32e23667d",
+    "url": "/static/js/main.bbddb039.chunk.js"
+  },
+  {
+    "revision": "5df311a9da0e9ce06ce4",
+    "url": "/static/js/runtime-main.b308e096.js"
+  }
+]);
